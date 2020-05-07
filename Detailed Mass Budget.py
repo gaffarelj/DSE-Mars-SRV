3ed4d59f328bdc45aae308b8929b1caeccbf8cb7
meno2 <menno.berger@live.nl>
2020-05-07 15:39:57 +0200

    Changes to propellant tank mass. Waaayy lighter spacecraft.

--- Detailed Mass Budget.py
+++ Detailed Mass Budget.py
@@ -7,7 +7,7 @@ Created on Wed May  6 14:34:44 2020
 
 from math import * 
 
-k = "SPACEPLANE"
+k = "SSTO"
 
 Isp       = 450
 g         = 9.81
@@ -29,7 +29,19 @@ def ClassIest(DeltaV,Ve,m_tot,TWR,m_upper):
     m_tank = 0.15*m_prop    
     m_thr_str = 1.949*10**(-3)*(Fvac/4.448)**1.0687*0.453
     m_stage = 0.001148*m_upper
-    
+    print(m_tank)
+
+    rho_f = 423
+    rho_ox = 1140
+    Mf = m_prop/(1+1/3.8) #3.8 is F/o ratio
+    Vf = Mf/rho_f
+    Mox = m_prop - Mf
+    Vox = Mox/rho_ox
+    m_fuel_tank = (2.42-0.00271*rho_f*0.06242796047)*Vf*35.31466671**(0.8445+0.00047*rho_f*0.06242796047)*0.45359237
+    m_oxid_tank = (2.42-0.00271*rho_ox*0.06242796047)*Vox*35.31466671**(0.8445+0.00047*rho_ox*0.06242796047)*0.45359237
+    m_tank = m_oxid_tank+m_fuel_tank
+    print(m_tank,m_prop)
+
     if m_stage == 0 : 
     
         M_total = m_t + m_prop + m_tank + m_thr_str + m_eng + m_stage + m_RCS
@@ -48,11 +60,19 @@ def Class_I_spaceplane_est(DeltaV, Ve, m_tot, TWR, m_wing):
     m_frac = exp(DeltaV / Ve)
     m_prop = m_frac * m_tot / (1 + m_frac)
     m_RCS = 0.0126 * m_tot
-    Fvac = TWR * 3.7 * m_tot * 0.10 #Assumption: L/D of 10
+    Fvac = TWR * 3.7 * m_tot
     m_eng = 0.00514 * Fvac ** 0.92068
     m_tank = 0.15 * m_prop
     m_thr_str = 1.949 * 10 ** (-3) * (Fvac / 4.448) ** 1.0687 * 0.453
-
+    rho_f = 423
+    rho_ox = 1140
+    Mf = m_prop/(1+1/3.8) #3.8 is F/o ratio
+    Vf = Mf/rho_f
+    Mox = m_prop - Mf
+    Vox = Mox/rho_ox
+    m_fuel_tank = (2.42-0.00271*rho_f*0.06242796047)*Vf*35.31466671**(0.8445+0.00047*rho_f*0.06242796047)*0.45359237
+    m_oxid_tank = (2.42-0.00271*rho_ox*0.06242796047)*Vox*35.31466671**(0.8445+0.00047*rho_ox*0.06242796047)*0.45359237
+    m_tank = m_oxid_tank+m_fuel_tank
     M_total = m_t + m_prop + m_tank + m_thr_str + m_eng + m_RCS + m_wing
 
     M_dry = m_t + m_tank + m_eng + m_thr_str + m_wing
@@ -141,9 +161,9 @@ def takeoff_wing_sizing_shuttle_like(M_takeoff,takeoff_mach,takeoff_cl):
     rho = 0.02 #Density at surface on Mars
     vel = 240 * takeoff_mach #takeoff speed as function of speed of sound (240) in m/s
     S = M_takeoff*3.7/(0.5*rho*vel**2*takeoff_cl)
-    b = 30.5/874.5 * S #Taken from the wing planform of the spaceshuttle, and scaled accordingly. Also, full span, not 1 wing
+    b = 30.5/874.5* 8 * S #Adjusted manually from spaceshuttle planform
     taper = 0.2 #Roughly, from spaceshuttle planform again
-    c_root = 0.5*0.5*b/(S*1.2)
+    c_root = S / (0.5*b)*(1/(1+taper))
     c_tip = taper * c_root
     return S, b, c_root, c_tip
 
@@ -151,30 +171,33 @@ def takeoff_wing_sizing_shuttle_like(M_takeoff,takeoff_mach,takeoff_cl):
 def AVID_wing_mass( M_land , b , S_exp , c_root , tc ):
     M_land = M_land * 2.205
     b = b / 0.3048
-    S_exp = S_exp / (0.3048**2) * 0.9 #90% of wing exposed
+    print("Wing Area:", S_exp)
+    S_exp = S_exp / (0.3048**2) * 0.8 #90% of wing exposed
+
     c_root = c_root / 0.3048
-    Mwing = 1575*((M_land*3.75*b*S_exp)/(c_root*tc*10**9))**0.67
-    Mwing = Mwing * 0.4536
-    print(Mwing)
-    Mwing = 1.498*S_exp/(0.3048**2)**1.176
-    Mwing = Mwing * 0.4536
-    print(Mwing)
-    print(" ")
+
+    #Mwing = 1575*((M_land*3.75*b*S_exp)/(c_root*tc*10**9))**0.67
+    #Mwing = Mwing * 0.4536
+
+    Mwing = 1.498 * S_exp **1.176 * 0.4536 * 0.2
+    print("Wing Mass:", Mwing)
+
     return Mwing
 
 if k == "SPACEPLANE":
     m_upper = 0
+    TWR = 0.15
     DeltaV = (DV1 + DV2) * f - 1000 # Assumed no DeltaV for landing
     m_tot = Mass_caps
     mt_margin = 0.1
     mts_margin = 0.1
     me_margin = 0.1
     mc_margin = 0.25
-    m_wing = 10000
+    m_wing = 1000
 
     m_frac, m_prop, m_RCS, Fvac, m_eng, m_tank, m_thr_str, M_total, M_dry, m_wing = Class_I_spaceplane_est(DeltaV,Ve,m_tot,TWR,m_wing)
 
-    print(m_frac, m_prop, m_RCS, Fvac, m_eng, m_tank, m_thr_str, M_total, M_dry,m_wing)
+    print(m_frac, m_prop, m_RCS, Fvac, m_eng, m_tank, m_thr_str, M_total, M_dry, m_wing)
 
     S,b,c_root,c_tip = takeoff_wing_sizing_shuttle_like(M_total,0.8,1.5)
 
