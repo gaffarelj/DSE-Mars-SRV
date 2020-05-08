b118a52523756ced53d06321c466d656a2aaa062
DimitrisApO <61149155+DimitrisApO@users.noreply.github.com>
2020-05-08 12:19:12 +0200

    Add files via upload

--- Detailed Mass Budget.py
+++ Detailed Mass Budget.py
@@ -12,14 +12,28 @@ k = "2_stage"
 N_crew = 6
 N_days = 4 
 conv   = 0.4535
+fcom   = 0.25 
+fEPs   = 0.25 
+fls    = 0.4 
+Klg    = 0.033
 
-m_EPS          = (3030*N_crew/7)*conv 
+m_fuel_cells   = (3030*N_crew/7)*conv 
+m_bat          = 216 
+
+m_EPS          = m_fuel_cells + m_bat 
+
 m_Life_support = (2444*N_crew/7 + 645*N_crew + 86.4*N_days)*conv 
+
 m_Com          = (131*N_days/7 + 1400*N_crew/7)*conv
 m_data_hand    = (302 + 828*N_days/7 + 1010*N_crew/7)*conv 
 m_GNC          = (242 + 108*N_days/7 + 617*N_crew/7)*conv 
 
-m_cabing       = (28.31*(39.66*(N_crew*N_days)**1.002)**0.6916)*conv
+m_av           = m_GNC + m_data_hand + m_Com*(1-fcom)
+
+
+
+m_cabin       = (28.31*(39.66*(N_crew*N_days)**1.002)**0.6916)*conv
+#m_pl_doors    = (0.257*barea/2)* conv 
 
 
 m_pl           = 1000
@@ -31,21 +45,22 @@ g         = 9.81
 f         = 1
 Ve        = Isp*g
 TWR       = 1.5
-Mass_caps = 6592.38
-DV1       = 2844*f
-DV2       = 3884*f 
+Mass_caps = m_EPS*(1-fEPs) + m_Life_support*(1-fls) + m_av + m_cabin + m_pl + m_payl_cont 
+DV1       = 2200*f
+DV2       = 3200*f 
 
-mt_margin = 0.2
-mts_margin = 0.2
-me_margin  = 0.2
-mc_margin    = 0.2 
+mt_margin   = 0.2
+mts_margin  = 0.2
+me_margin   = 0.2
+mc_margin   = 0.2 
+mlg_margin  = 0.2
  
 def ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2):
     
+    klg     = 0.033
     m_t     = Mass_caps    
     m_frac = exp(DeltaV/Ve)    
-    m_prop = m_frac*m_tot/(1+m_frac)         
-    m_tank = 0.15*m_prop    
+    m_prop = m_frac*m_tot/(1+m_frac)          
     m_stage = 0.001148*m_upper
    
     
@@ -68,8 +83,11 @@ def ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2):
         Fvac   = TWR*3.7*m_tot  
         m_RCS  = 0.0126*m_tot
         m_eng  = 0.00514*Fvac**0.92068
-        m_thr_str = 1.949*10**(-3)*(Fvac/4.448)**1.0687*0.453
-        M_total = m_t + m_prop + m_tank + m_thr_str + m_eng + m_stage + m_RCS    
+        #m_eng  = 1.866**(-10)*Fvac + 0.00130*Fvac+77.4
+        m_thr_str = (1.949*10**(-3)*(Fvac/4.448)**1.0687)*0.453
+        M_dry     = m_eng + m_thr_str + m_tank + Mass_caps
+        Mlg       = klg * M_dry*1.15 
+        M_total = m_t + m_prop + m_tank + m_thr_str + m_eng + m_stage + m_RCS + Mlg   
     
     if m_stage != 0 : 
         
@@ -78,30 +96,33 @@ def ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2):
         m_RCS  = 0.0126*m_tot2
         m_eng  = 0.00514*Fvac**0.92068 
         m_thr_str = 1.949*10**(-3)*(Fvac/4.448)**1.0687*0.453
-        M_total =  m_tot + m_prop + m_tank + m_thr_str + m_eng + m_stage + m_RCS    
+        M_dry     = m_eng + m_thr_str + m_tank + Mass_caps
+        Mlg       = klg * M_dry*1.15  
+        M_total =  m_tot + m_prop + m_tank + m_thr_str + m_eng + m_stage + m_RCS + Mlg    
     
-    return m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total
+    return m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total,Mlg 
 
 if k == "SSTO" :
     m_tot2 =  0 
     m_upper = 0 
     DeltaV  = (DV1 + DV2)*f
+    DeltaV  = 5400 
     m_tot   = Mass_caps
     
-    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total  = ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2)
+    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total,Mlg  = ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2)
     
     #print(m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total)
     
     m_tot = M_total 
     
-    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new  = ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2)
+    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new,Mlg = ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2)
     
     while M_total_new > M_total + 0.001 : 
         M_total = M_total_new 
         
         m_tot = M_total
         
-        m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new  = ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2)
+        m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new,Mlg  = ClassIest(DeltaV,Ve,m_tot,TWR,m_upper,m_tot2)
         
         print(M_total_new,"--------------",M_total)
     
@@ -111,8 +132,9 @@ if k == "SSTO" :
     m_thr_str = m_thr_str*(1+mts_margin)
     m_tank = m_tank*(1+mt_margin) 
     m_caps = (mc_margin+1)* Mass_caps
+    Mlg    = (1+mlg_margin)*Mlg 
     
-    M_dry  = m_eng + m_thr_str + m_tank + m_caps
+    M_dry  = m_eng + m_thr_str + m_tank + m_caps + Mlg 
     
     print(M_dry,m_prop,M_total,m_tank,m_thr_str,m_eng)
     
@@ -124,33 +146,24 @@ if k =="2_stage" :
     
     # 1st stage calculation 
     
-    m_frac1,m_prop1,m_RCS1,Fvac1,m_eng1,m_tank1,m_thr_str1,m_stage1,M_total1 = ClassIest(DV1,Ve,m_tot,TWR,m_upper,m_tot2)
+    m_frac1,m_prop1,m_RCS1,Fvac1,m_eng1,m_tank1,m_thr_str1,m_stage1,M_total1,Mlg1 = ClassIest(DV1,Ve,m_tot,TWR,m_upper,m_tot2)
     
     m_upper = M_total1 
     m_tot   = M_total1 
     m_tot2    = 0 
-    #print(M_total1)
-    print(m_eng1)
     
-    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total  = ClassIest(DV2,Ve,m_tot,TWR,m_upper,m_tot2)
+    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total,Mlg  = ClassIest(DV2,Ve,m_tot,TWR,m_upper,m_tot2)
     
     m_tot = M_total1
     m_upper = 0 
     m_tot2    = M_total 
-    #print(M_total)
-    print(m_eng)
     
-    m_frac1,m_prop1,m_RCS1,Fvac1,m_eng1,m_tank1,m_thr_str1,m_stage1,M_total1 = ClassIest(DV1,Ve,m_tot,TWR,m_upper,m_tot2)
+    m_frac1,m_prop1,m_RCS1,Fvac1,m_eng1,m_tank1,m_thr_str1,m_stage1,M_total1,Mlg1 = ClassIest(DV1,Ve,m_tot,TWR,m_upper,m_tot2)
     
     m_upper = M_total1 
     m_tot   = M_total1 
-    #print(M_total1)
-    print(m_eng1)
-    
-    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new  = ClassIest(DV2,Ve,m_tot,TWR,m_upper,m_tot2)
-    
-    #print(M_total_new)
-    print(m_eng)
+
+    m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new,Mlg  = ClassIest(DV2,Ve,m_tot,TWR,m_upper,m_tot2)
     
     
     while M_total_new > M_total + 0.001 : 
@@ -159,12 +172,12 @@ if k =="2_stage" :
         m_tot2    = M_total_new 
         m_upper = 0 
         
-        m_frac1,m_prop1,m_RCS1,Fvac1,m_eng1,m_tank1,m_thr_str1,m_stage1,M_total1= ClassIest(DV1,Ve,m_tot,TWR,m_upper,m_tot2)
+        m_frac1,m_prop1,m_RCS1,Fvac1,m_eng1,m_tank1,m_thr_str1,m_stage1,M_total1,Mlg1= ClassIest(DV1,Ve,m_tot,TWR,m_upper,m_tot2)
     
         m_upper = M_total1 
         m_tot   = M_total1 
         
-        m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new  = ClassIest(DV2,Ve,m_tot,TWR,m_upper,m_tot2)
+        m_frac,m_prop,m_RCS,Fvac,m_eng,m_tank,m_thr_str,m_stage,M_total_new,Mlg  = ClassIest(DV2,Ve,m_tot,TWR,m_upper,m_tot2)
         
         print(M_total_new,"-------",M_total)
         
@@ -172,17 +185,19 @@ if k =="2_stage" :
     m_thr_str = m_thr_str*(1+mts_margin)
     m_tank = m_tank*(1+mt_margin) 
     m_caps = (mc_margin+1)* Mass_caps
+    Mlg    = (1+mlg_margin)*Mlg 
     
     M_dry  = m_eng + m_thr_str + m_tank 
     
     m_eng1  = (1+me_margin) * m_eng1 
     m_thr_str1 = m_thr_str1*(1+mts_margin)
     m_tank1 = m_tank1*(1+mt_margin) 
+    Mlg1    = (1+mlg_margin)*Mlg1 
     
     
-    M_dry1  = m_eng1 + m_thr_str1 + m_tank1 
+    M_dry1  = m_eng1 + m_thr_str1 + m_tank1 + Mlg1
     
-    M_dry_tot = M_dry1 + M_dry + m_caps
+    M_dry_tot = M_dry1 + M_dry + m_caps + Mlg
     
     print("---------------------2st stage-------------")
     print("Tank stage 2", m_tank1)
