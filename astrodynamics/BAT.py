06f60a9d5faf36a8427d92631bcbcd40989ab02e
jgaffarel <j.y.p.gaffarel@student.tudelft.nl>
2020-06-03 11:50:17 +0200

    Implemented BAT to chute

--- astrodynamics/BAT.py
+++ astrodynamics/BAT.py
@@ -328,7 +328,7 @@ def ascent_sim(tb=148.7274456555216,initial_tilt=3.2,i_base=42.5,h0=-3*10**3,d=7
 
 			# Progress indication
 			progress = round(Z[-1]/h_phasing*100)
-			print(progress, "%", sep="", end="\r")
+			print("Ascent progress: ", progress, "%", sep="", end="\r")
 
 			#update velocities
 			Vx.append(Vxnew)
