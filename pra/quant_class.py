07a25f75bffdbb2ca5914351ccb37b38b5e62360
Lasse <lasse@landergren.dk>
2020-06-15 17:01:12 +0200

    changed quant class to allow for ptinitng table and other additions

--- pra/quant_class.py
+++ pra/quant_class.py
@@ -22,23 +22,38 @@ class comp:
         self.val_comp = False
         self.prob_val = 0
     
-    def prob(self, c_dict, e_dict):
+    def prob(self, pra):
         if not(self.val_comp):
             pr = 1
             for key in self.event_list:
-                if key in e_dict:
-                    pr *= 1- e_dict[key].prob
-                elif key in c_dict:
-                    if c_dict[key].val_comp:
-                        pr *= 1-prob_val
-                        
+                if key in pra.e_dict:
+                    pr *= 1- pra.e_dict[key].prob
+                elif key in pra.c_dict:
+                    if pra.c_dict[key].val_comp:
+                        pr *= 1-pra.c_dict[key].prob_val     
                     else:
-                        pr *= 1-c_dict[key].prob(c_dict,e_dict)
+                        pr *= 1-pra.c_dict[key].prob(pra)
+                elif key[:4] == "all_":
+                    if key[4:] in pra.con_list:
+                        pr *= 1-pra.proability[pra.con_list.index(key[4:])]
             self.prob_val = 1- pr
             self.val_comp = True
             print(self.prob_val)
         return self.prob_val
-                
+    
+    def name_func(self, pra):
+        self.name = "("
+        for key in self.event_list:
+            if key in pra.e_dict:
+                self.name += key
+            elif key in pra.c_dict:
+                self.name += pra.c_dict[key].name_func(pra)
+            elif key[:4] == "all_":
+                self.name += key
+            if key != self.event_list[-1]:
+                self.name += " AND "
+        self.name += ")"
+        return self.name
 
 
 
@@ -74,11 +89,30 @@ class PRA:
                 
                 if comp.con_n == i:
                     print(key)
-                    pr *= comp.prob(self.c_dict,self.e_dict)
+                    pr *= comp.prob(self)
             self.proability[i] = pr
+    def gen_table(self,caption, label):
+        print("\\begin{table}[H]")
+        print("\\centering")
+        print("\\caption{" + caption + "}")
+        print("\\begin{tabular}{|l|c|c|} \\hline")
+        for i in range(len(self.con_list)):
+            con = self.con_list[i]
+            print("\\multicolumn{2}{|c|}{\\textbf{" + con + "}} & " + str(round(1/(1-self.proability[i]))) + "\\\\\\hline")
+            for key in self.e_dict:
+                event = self.e_dict[key]
+                if event.con == con:
+                    print(key + " & " + event.desc + " & " + str(round(1/(1-event.prob))) + "\\\\\\hline")
+            
+            for key in self.c_dict:
+                comp = self.c_dict[key]
+                if comp.con == con:
+                    print(comp.name_func(self) + " & " + comp.desc + " & " + str(round(1/(1-comp.prob_val))) + "\\\\\\hline")
+        print("\\label{tab:"+label+"}")
+        print("\end{tabular}")
+        print("\end{table}")
 
 
-        
 
 
 
